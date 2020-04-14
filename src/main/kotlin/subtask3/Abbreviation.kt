eb438e6c6f624697e35fbb65a02d708679fd592a
vlad-gal <vlgalathevich@gmail.com>
2020-04-14 20:52:10 +0300

    feat: implement Abbreviation

--- src/main/kotlin/subtask3/Abbreviation.kt
+++ src/main/kotlin/subtask3/Abbreviation.kt
@@ -4,6 +4,14 @@ class Abbreviation {
 
     // TODO: Complete the following function
     fun abbreviationFromA(a: String, b: String): String {
-        throw NotImplementedError("Not implemented")
+        val arrayA = a.toUpperCase().toList()
+        val arrayB = b.toList()
+        val i = arrayA.intersect(arrayB).toList()
+        if (i == arrayB){
+            return "YES"
+        } else{
+            return "NO"
+        }
     }
 }
+
